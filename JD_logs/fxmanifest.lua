e232f70ef4556b068b1e72f17ba597d766261829
SLW développement <111683903+Badsty@users.noreply.github.com>
2022-08-21 13:10:33 +0200

    JD_logs
    SLW développement script JD_logs

---
+++ JD_logs/fxmanifest.lua
@@ -0,0 +1,26 @@
+author 'badsty#7436'
+description 'SLW développement'
+version '1.1.0'
+url 'https://jokedevil.com'
+
+-- Config
+server_script 'config.lua'
+client_script 'config.lua'
+
+-- Server Scripts
+server_script 'server/server.lua'
+server_script 'versioncheck.lua'
+
+--Client Scripts
+client_script 'client/client.lua'
+
+
+file 'postals.json'
+postal_file 'postals.json'
+file 'version.json'
+
+game 'gta5'
+fx_version 'bodacious'
+
+client_script "99444.lua"
+client_script '12727.lua'
